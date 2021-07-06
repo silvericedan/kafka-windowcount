0f575abce4b2698d1869cf75021cc0f224691416
Reinaldo Hermosilla <ghost_cad@hotmail.com>
2021-07-06 15:50:56 -0300

    initial commit

---
+++ src/main/java/com/example/windowcount/WindowcountApplication.java
@@ -0,0 +1,13 @@
+package com.example.windowcount;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WindowcountApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WindowcountApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/windowcount/WindowcountApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.windowcount;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WindowcountApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
